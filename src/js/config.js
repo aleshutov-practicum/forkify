9de6aca7178b79d4e01e3085bb9b9c44373ba4ce
Alexander Shutov <alexander.shutov@peloton.com>
2025-10-31 14:52:35 -0600

    Initial

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 1000;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bdce5d71-992e-42cc-a33e-96c2093d2587';
+export const MODAL_CLOSE_SEC = 2.5;
